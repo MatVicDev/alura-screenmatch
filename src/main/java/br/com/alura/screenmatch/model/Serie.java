1315d9016da4c2fa36ee7374640a60b438081235
Matheus Victor <matheusvic013@gmail.com>
2024-08-01 15:20:09 -0300

    Listando os episódios no log

--- src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -25,7 +25,7 @@ public class Serie {
     private String poster;
     private String sinopse;
 
-    @OneToMany(mappedBy = "serie", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "serie", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Episodio> episodios = new ArrayList<>();
 
     public Serie() {
@@ -122,6 +122,7 @@ public class Serie {
                 " avaliação = " + avaliacao + '\'' +
                 " atores = " + atores + '\'' +
                 " poster = " + poster + '\'' +
-                " sinopse = " + sinopse + '\'';
+                " sinopse = " + sinopse + '\'' +
+                " episódios = " + episodios + '\'';
     }
 }
