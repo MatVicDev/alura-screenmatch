9f7806a972d863706e2d0c833f19f6584d5c0729
Matheus Victor <matheusvic013@gmail.com>
2024-08-01 15:16:32 -0300

    Relacionando episodios a série

--- src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -110,6 +110,7 @@ public class Serie {
     }
 
     public void setEpisodios(List<Episodio> episodios) {
+        episodios.forEach(e -> e.setSerie(this));
         this.episodios = episodios;
     }
 
